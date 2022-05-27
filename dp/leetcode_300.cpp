4142ecaa3c4888e755247635a5bc0889646d7c2f
Bite Zhang <65553809+holy-tree@users.noreply.github.com>
2022-05-27 11:05:06 +0800

    Create leetcode_300.cpp

---
+++ dp/leetcode_300.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        int n = nums.size();
+        int dp[n];
+        dp[0] = 1;
+        int ans = 1;
+        for(int i = 1; i < n; i++){
+            dp[i] = 1;
+            for(int j = i-1; j >= 0; j--){
+                if(nums[i] > nums[j]){
+                    dp[i] = max(dp[i], dp[j]+1);
+                }
+            }
+            ans = max(ans, dp[i]);
+        }
+        return ans;
+    }
+};
